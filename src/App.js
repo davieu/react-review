32d4dad11f85dd6783f45abcc23981c6880bd298
davieu <davisumana@gmail.com>
2019-03-19 17:12:27 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28},
+      { name: 'Manu', age: 29},
+      { name: 'Stephanie', age: 26}
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log('was clicked')
+  }
+
   render() {
     return (
       <div className="App">
-       
+        <h1>Hi I'm a react app</h1>
+        <p>hello</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }
